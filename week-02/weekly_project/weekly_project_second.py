102d725f2f5a863a562d95c047b5b9c98d09c1b4
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-03-23 15:47:56 +0100

    WEEKLY PROJECT DONEgit add .git add .git add .git add .git add .git add .git add .git add .

--- week-02/weekly_project/weekly_project_second.py
+++ week-02/weekly_project/weekly_project_second.py
@@ -8,10 +8,33 @@ all_strings = 'dog goat dad duck doodle never'
 # for i in range(len(all_strings)):
 #     all_strings = all_strings.replace(" ","")
 # #doggoatdadduckdoodlenever
-for i in range(len(all_strings)):
-    string = all_strings[i-3:i]
-    backwards = ""
-    for i in range(len(string), 0, -1):
-        backwards += string[i-1]
-    if(string == backwards):
-        print(string)
+def palindrome_searcher(put_strings_here):
+    output=[]
+    for x in range(3,6):
+        for i in range(len(put_strings_here)):
+            string = all_strings[i-x:i]
+            backwards = ""
+            for j in range(len(string), 0, -1):
+                backwards += string[j-1]
+            if(string == backwards and len(string) == x):
+                output.append(string)
+    return output
+
+
+stringer = palindrome_searcher('dog goat dad duck doodle never')
+print(stringer)
+# for i in range(len(all_strings)):
+#     string = all_strings[i-4:i]
+#     backwards = ""
+#     for j in range(len(string), 0, -1):
+#         backwards += string[j-1]
+#     if(string == backwards):
+#         print(string)
+#
+# for i in range(len(all_strings)):
+#     string = all_strings[i-5:i]
+#     backwards = ""
+#     for j in range(len(string), 0, -1):
+#         backwards += string[j-1]
+#     if(string == backwards):
+#         print(string)
