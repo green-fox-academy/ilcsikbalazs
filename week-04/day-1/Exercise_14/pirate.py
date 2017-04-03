1e3b44d0c2d1b30a0b9733639cccc5505cb8ef5f
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-04-03 17:10:15 +0200

    This version is not working, i have to redo the whole thing.

--- week-04/day-1/Exercise_14/pirate.py
+++ week-04/day-1/Exercise_14/pirate.py
@@ -3,43 +3,45 @@ import random
 
 class Pirate:
 
-    def __init__(self, toxic_lvl=0, dies=False):
+    def __init__(self, toxic_lvl=0, dies=False, passed_out=False):
         self.toxic_lvl = toxic_lvl
         self.dies = dies
+        self.passed_out = passed_out
 
     def drink_some_rum(self):
         self.toxic_lvl += 1
-        if self.toxic_lvl >= 4 and self.dies==False:
-            print("Pirate passed out! ")
-        elif self.dies == True:
-            print("You already died! ")
+        if self.toxic_lvl >= 4:
+            self.pass_out() = True
 
     def hows_it_going_mate(self):
-        if self.toxic_lvl < 4 and self.dies==False:
+        if self.toxic_lvl < 4:
             print("Pour me anudder!")
-        elif self.toxic_lvl >= 4 and self.dies==False:
+        elif self.toxic_lvl >= 4:
             print("Arghh, I'ma Pirate. How d'ya d'ink its goin?")
-        elif self.dies == True:
-            print("You already died! ")
 
     def die(self):
         self.dies = True
-        print("Pirate died! ")
+        print("Your pirate died!")
 
     def brawl(self,other_pirate):
         chance = random.randint(1,3)
-        if (self.dies==False) and (other_pirate.dies==False):
-            if chance == 1:
-                self.dies = True
-                print("Your pirate died!")
-            elif chance == 2:
-                other_pirate.dies = True
-                print("Enemy pirate died!")
-            elif chance == 3:
-                print("Both passed out!")
+        if chance == 1:
+            self.die()
+        elif chance == 2:
+            other_pirate.die()
+            print("Enemy pirate died!")
+        elif chance == 3:
+            self.pass_out()
+            other_pirate.pass_out()
+            print("Both passed out!")
+
+    def pass_out(self):
+        self.passed_out = True
+        print("Pirate passed out")
+
 
 
 jack = Pirate()
-bluebeard = Pirate()
+blue = Pirate()
 
-jack.brawl(bluebeard)
+# class Ship:
