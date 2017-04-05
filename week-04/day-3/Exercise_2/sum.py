7f3b78d31ea2a33e372ab1b875dd776f2d06b1bd
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-04-05 13:38:18 +0200

    refactor(Sum())

--- week-04/day-3/Exercise_2/sum.py
+++ week-04/day-3/Exercise_2/sum.py
@@ -17,8 +17,11 @@ class Sum:
     def __init__(self):
         self.sum_list=[]
     
-    def add_number(self,number):
-        self.sum_list.append(number)
+    def add_number(self,number=None):
+        if number == None:
+            return self.sum_list
+        else:
+            self.sum_list.append(number)
 
     def return_list(self):
         return self.sum_list    
