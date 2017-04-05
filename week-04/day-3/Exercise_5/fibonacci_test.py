457f5166d236365eec8f815f8c515f6dcfa94e9a
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-04-05 15:32:58 +0200

    test(fibonacci): tested on 0 and 1 as well. Worked

--- week-04/day-3/Exercise_5/fibonacci_test.py
+++ week-04/day-3/Exercise_5/fibonacci_test.py
@@ -7,5 +7,15 @@ class Test_Fibonacci(unittest.TestCase):
         fib.fibonacci(10)
         self.assertEquals(fib.fibonacci(10), 55)
 
+    def test_fibonacci_zero(self):
+        fib = Fibonacci()
+        fib.fibonacci(0)
+        self.assertEquals(fib.fibonacci(0), 0)
+
+    def test_fibonacci_one(self):
+        fib = Fibonacci()
+        fib.fibonacci(1)
+        self.assertEquals(fib.fibonacci(1), 1)
+
 if __name__ == "__main__":
     unittest.main()
