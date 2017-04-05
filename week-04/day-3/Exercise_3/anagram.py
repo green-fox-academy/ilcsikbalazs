e717217c5c0b909f6eaf3fc5cc397aea998aa8db
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-04-05 14:26:45 +0200

    refactor(Anagram()) anagram checker can compare two same sized strings.

--- week-04/day-3/Exercise_3/anagram.py
+++ week-04/day-3/Exercise_3/anagram.py
@@ -6,17 +6,29 @@
 # Create a test for that.
 
 class Anagram:
-    def __init__(self,string_a, string_b, result = False):
-        self.result = False
-        self.string_a = []        
-        for i in string_a:
-            self.string_a
+    def __init__(self,string_a="test", string_b="tste", is_anagram = False):
+        self.is_anagram = False
+        # self.string_a = []        
+        # for i in string_a:
+        #     self.string_a
+        self.string_b = string_b
+        self.string_a = string_a       
+        
     
     def anagram_checker(self):
-        
+        if len(self.string_a) != len(self.string_b):
+            self.is_anagram = False
+            return self.is_anagram
+            
         for i in self.string_a:
             if i in self.string_b:
-                i
+                self.is_anagram = True
+                return self.is_anagram
+            else: 
+                self.is_anagram = False
+                return self.is_anagram
 
+anagrams = Anagram()
+anagrams.anagram_checker()
 
 
--- week-04/day-3/Exercise_3/anagram_test.py
+++ week-04/day-3/Exercise_3/anagram_test.py
@@ -5,7 +5,7 @@ class Test_anagram(unittest.TestCase):
     def test_anagram_print(self):
         anagrams = Anagram("test", "sett")
         anagrams.anagram_checker()
-        self.assertEquals(anagrams.anagram_checker(), "test")
+        self.assertEquals(anagrams.anagram_checker(), True)
 
 if __name__ == "__main__":
     unittest.main()
