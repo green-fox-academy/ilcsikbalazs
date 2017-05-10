809bad9fc3108ecd7ffeb7e9d41d85541752734b
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-05-10 17:58:02 +0200

    refactor candshop added extras too

--- week-08/day-3/candyShop/game.js
+++ week-08/day-3/candyShop/game.js
@@ -4,10 +4,13 @@ var lollypops = document.querySelector('.lollypops')
 var buyLollypop = document.querySelector('.buy-lollypops');
 var speed = document.querySelector('.speed');
 var makeItRain = document.querySelector('.candy-machine');
+var speedContentInt = 0;
+var numberOfLollypops = lollypops.textContent.length/2;
 
 function addCandy() {
-    candies.textContent++;
-    callback();
+    var candiesNumber = parseInt(candies.textContent);
+    candiesNumber++;
+    candies.textContent = candiesNumber;
 }
 
 candyButton.addEventListener('click', function(){
@@ -18,15 +21,36 @@ buyLollypop.addEventListener('click', function(){
     if (candies.textContent >= 10) {
         candies.textContent -= 10;
         lollypops.textContent += '🍭';
-        var numberOfLollypops = lollypops.textContent.length/2;
+        numberOfLollypops = lollypops.textContent.length/2;
     }
     if (numberOfLollypops/10 >= 1) {
         speed.textContent = parseInt(numberOfLollypops/10);
+        speedContentInt = parseInt(speed.textContent);
     }
 })
 
-function callback() {
-    var timer = setTimeout(addCandy, 5000);
-}
+setInterval(function() {
+    if(speedContentInt >= 1) {
+        var candiesNumber = parseInt(candies.textContent);
+        candiesNumber += speedContentInt;
+        candies.textContent = candiesNumber;
+    }
+}, 1000);
 
-// makeItRain.addEventListener('click', )
+makeItRain.addEventListener('click', function(){
+    if (numberOfLollypops >= 30) {
+        lollypops.textContent = lollypops.textContent.substring(0, lollypops.textContent.length-60);
+        numberOfLollypops = lollypops.textContent.length/2;
+        var candiesNumber = parseInt(candies.textContent);
+        speedContentInt = speedContentInt * 10;
+        speed.textContent = speedContentInt;
+        candiesNumber += speedContentInt;
+        candies.textContent = candiesNumber;
+        setTimeout(function () {
+            speed.textContent = parseInt(numberOfLollypops/10);
+            speedContentInt = parseInt(speed.textContent);
+            speedContentInt = speedContentInt;
+            speed.textContent = speedContentInt;
+        }, 2000);
+    }
+})
