758335618a0ecd10de5951ba7ab62c13a36906b8
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-05-11 13:23:14 +0200

    ex1 ready

--- week-08/day-4/exercise1/script.js
+++ week-08/day-4/exercise1/script.js
@@ -1,23 +1,20 @@
+'use strict';
+
 var div = document.querySelector('div');
-
-
 
 var httpRequest = new XMLHttpRequest();
 httpRequest.onreadystatechange = function() {
     if(httpRequest.readyState === 4 && httpRequest.status === 200) {
         var pictures = JSON.parse(httpRequest.response);
         console.log(pictures);
-        for(let i = 0; i < 2; i++) {
-            var image = pictures.data[i].images.downsized_still.url;
-            var images = document.createElement('img');
-            images.setAttribute('src', image);
-            images.addEventListener('click', function(){
-                gif = pictures.data[i].images.original.url;
-                images.setAttribute('src', gif);
-            })
+        for(let i = 0; i < 16; i++) {
+            let images = document.createElement('img');
+            images.setAttribute('src', pictures.data[i].images.downsized_still.url);
             div.appendChild(images);
+            images.addEventListener('click', function(){
+                images.setAttribute('src', pictures.data[i].images.original.url);
+            })
         }
-        
     }
 }
 httpRequest.open('GET', 'http://api.giphy.com/v1/gifs/search?q=meme&api_key=dc6zaTOxFJmzC');
