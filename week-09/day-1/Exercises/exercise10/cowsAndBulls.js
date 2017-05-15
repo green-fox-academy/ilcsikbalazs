932c512f76387050e208eb73a4abf50a6cba23f7
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-05-15 20:47:17 +0200

    feat(ex10) random generator works, game created

--- week-09/day-1/Exercises/exercise10/cowsAndBulls.js
+++ week-09/day-1/Exercises/exercise10/cowsAndBulls.js
@@ -1,22 +1,27 @@
 'use strict';
 
-// Create a class what is capable of playing exactly one game of Cows and Bulls (CAB).
-// The player have to guess a 4 digit number. For every digit that the player guessed correctly in the
-// correct place, they have a “cow”. For every digit the player guessed correctly in the wrong place is a “bull.”
-
-// The CAB object should have a random 4 digit number, which is the goal to guess.
-// The CAB object should have a state where the game state is stored (playing, finished).
-// The CAB object should have a counter where it counts the guesses.
-// The CAB object should have a guess method, which returns a string of the guess result
-// All methods, including constructor should be tested
+let RandomNumbers = function() {
+    let randomArray = [];
+    while(true) {
+        let randomNumber = Math.floor(Math.random() * 10);
+        if(randomNumber !== 0 && randomNumber != 10 && randomArray.includes(randomNumber) === false) {
+            randomArray.push(randomNumber);
+        }
+        if(randomArray.length === 4) {
+            break;
+        }
+    }
+    return randomArray;
+}
 
 let CAB = {
-    numbers: [1,6,4,8],
+    numbers: RandomNumbers(),
     playing: true,
     guessCounter: 0,
     GuessMethod: function(guess) {
         let strGuess = "";
         let strNumbers = "";
+        let getResult = [];
         guess.forEach(function(element) {
             strGuess += element;
         });
@@ -25,14 +30,29 @@ let CAB = {
         });
         guess.forEach(function(element, index) {
             if(strNumbers[index] === strGuess[index]) {
-                console.log("cow")
+                getResult.push("cow");
             }
-
             if(this.numbers.includes(element) && strNumbers[index] != strGuess[index]) {
-                console.log("bull");
+                getResult.push("bull");
             }
         }.bind(this));
+        return getResult;
     }
 }
 
-CAB.GuessMethod([8,1,6,4])
+//game
+while(CAB.playing) {
+    console.log(CAB.GuessMethod([1,2,3,4]));
+    if(CAB.guessCounter === 15) {
+        console.log('You lose');
+        CAB.playing = false;
+    }
+    if(CAB.GuessMethod([1,2,3,4]) === ['cow','cow','cow','cow']) {//input needed
+        console.log('You won');
+        CAB.playing = false;
+    } else {
+        CAB.guessCounter++;
+    }
+
+}
+console.log(CAB.GuessMethod([4,5,3,1]));
