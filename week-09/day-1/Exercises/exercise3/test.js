81a4ca8f06fd753501f63f9b082f6081c1c4db2d
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-05-15 12:03:50 +0200

    refactor(sum) 4 tests are running

--- week-09/day-1/Exercises/exercise3/test.js
+++ week-09/day-1/Exercises/exercise3/test.js
@@ -6,15 +6,15 @@ let sum = require('./sum.js');
 test('test sum', function(t) {
     let integers0 = [];
     let integers1 = [1];
-    let integers123 = [1,2,3];
+    let integersMultyElements = [1,2,3];
     let sumOf0 = sum(integers0);
     let sumOf1 = sum(integers1);
-    let sumOf123 = sum(integers123);
+    let sumOfMultyElements = sum(integersMultyElements);
 
     t.ok(typeof sum === 'function', 'test if sum is a function');
     t.equal(sumOf0, 0, 'test if returns 0 to an empty list');
     t.equal(sumOf1, 1, 'test if returns 1 to a one element list');
-    t.equal(sumOf123, 6, 'test if returns sum of 1 + 2 + 3');
+    t.equal(sumOfMultyElements, 6, 'test if runs on multyple element');
     t.end();
 })
 
