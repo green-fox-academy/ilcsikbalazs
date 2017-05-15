7d13d201d2e2f1ae8f5f4dd88f6207ecebb83c3e
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-05-15 17:18:53 +0200

    feat(ex7) last two tests are not done

--- week-09/day-1/Exercises/exercise7/extension.js
+++ week-09/day-1/Exercises/exercise7/extension.js
@@ -2,31 +2,66 @@
 
 // Adds a and b, returns as result
 const addNumbers = function(a, b) {
-  return 5;
+  if(typeof a === 'number' && typeof b === 'number') {
+    return a+b;
+  } else {
+    throw new Error('Invalid value');
+  }
 }
 
 // Returns the highest value from the three given params
 const maxOfThree = function(a, b, c) {
-  if (a > b) {
-    return a;
+  if(typeof a === 'number' && typeof b === 'number' && typeof c === 'number') {
+    if (a > b && a > c) {
+      return a;
+    } else if (c > a && c > b) {
+      return c;
+    } else if (b > a && b > c) {
+      return b;
+    }
   } else {
-    return c;
+    throw new Error('Invalid value');
   }
 }
 
 //Returns the median value of a list given as param
 const median = function(pool){
-  return(pool[(pool.length - 1) / 2]);
+  try {
+    pool.forEach(function(element) {
+      if(typeof element != 'number') {
+        throw new Error('Invalid value');
+      }
+    });
+    if(pool.length%2 === 0) {
+      return pool.sort()[pool.length/2];
+    } else {
+      return pool.sort()[(pool.length-1)/2];
+    }
+  } catch (error) {
+    return error.message;
+  }
 }
 
 // Returns true if the param is a vovel
 const isVovel = function(char){
-  return 'aeiouéáőűöüóí'.indexOf(char);
+  try {
+    if(char.length > 0 && char.length < 2) {
+      if('aeiouéáőűöüóí'.includes(char)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      throw new Error('Please, give me one character');
+    }
+  } catch (error) {
+    return error.message;
+  }
 }
 
 // Create a method that translates hungarian into the teve language
 const translate = function(hungarian) {
-  let text = hungarian.split('');
+  let text = toString(hungarian).split('');
   let teve = '';
   text.forEach(function(char){
     if (isVovel(char)){
