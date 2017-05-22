cf8318b6bca746a1238dd67a77b72320d8b68a77
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-05-22 15:45:06 +0200

    refactor(tennisgame3) getScore understood

--- week-10/day-1/TennisGame3.js
+++ week-10/day-1/TennisGame3.js
@@ -1,31 +1,31 @@
-var TennisGame3 = function(p1N, p2N) {
-    this.p2 = 0;
+var TennisGame3 = function(player1Name, player2Name) {
     this.p1 = 0;
+    this.p2 = 0;
 
-    this.p1N = p1N;
-    this.p2N = p2N;
+    this.player1Name = player1Name;
+    this.player2Name = player2Name;
 };
 
 TennisGame3.prototype.getScore = function() {
-    var s;
+    var score;
     if ((this.p1 < 4 && this.p2 < 4) && (this.p1 + this.p2 < 6)) {
-        var p = ["Love", "Fifteen", "Thirty", "Forty"];
-        s = p[this.p1];
-        return (this.p1 == this.p2) ? s + "-All" : s + "-" + p[this.p2];
+        var position = ["Love", "Fifteen", "Thirty", "Forty"];
+        score = position[this.p1];
+        return (this.p1 === this.p2) ? score + "-All" : score + "-" + position[this.p2];
     } else {
-        if (this.p1 == this.p2)
+        if (this.p1 == this.p2) {
             return "Deuce";
-        s = this.p1 > this.p2 ? this.p1N : this.p2N;
-        return ((this.p1 - this.p2) * (this.p1 - this.p2) == 1) ? "Advantage " + s : "Win for " + s;
+        }
+        score = this.p1 > this.p2 ? this.player1Name : this.player2Name;
+        return ((this.p1 - this.p2) * (this.p1 - this.p2) === 1) ? "Advantage " + score : "Win for " + score;
     }
 };
 
 TennisGame3.prototype.wonPoint = function(playerName) {
-    if (playerName == "player1")
+    if (playerName === "player1")
         this.p1 += 1;
     else
         this.p2 += 1;
-
 };
 
 if (typeof window === "undefined") {
